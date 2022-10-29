36edf449a14081035575c45b62eda31951c66c4d
Chasseuse <90153466+Chasseuse@users.noreply.github.com>
2022-10-29 23:55:43 +0200

    7 commit

--- code/index.php
+++ code/index.php
@@ -1,2 +1,10 @@
 <?php
-echo phpinfo();
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+$order = & $very_bad_unclear_name;
+$order = " Hello " . $order;
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
