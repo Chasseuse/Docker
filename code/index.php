48de358b75ed5a773c39acc358df9604f602cddb
Chasseuse <90153466+Chasseuse@users.noreply.github.com>
2022-10-30 00:08:16 +0200

    8 commit

--- code/index.php
+++ code/index.php
@@ -8,3 +8,29 @@ $order = " Hello " . $order;
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+echo "<br>";
+
+$number = 1;
+echo $number;
+
+echo "<br>";
+
+$irr = 1.1;
+echo $irr;
+
+echo "<br>";
+
+echo 12;
+
+echo "<br>";
+
+echo 1 + 11;
+
+echo "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
+
+echo "<br>";
