f7e96ae5a7e1da785eb345d8b82435419b88b113
Chasseuse <90153466+Chasseuse@users.noreply.github.com>
2022-10-30 00:14:51 +0200

    9 commit

--- code/index.php
+++ code/index.php
@@ -34,3 +34,11 @@ $this_month = 1089.98;
 echo $last_month - $this_month;
 
 echo "<br>";
+
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo $days_per_language;
+
+echo "<br>";
